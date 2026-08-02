6ba9078e91660c615b110eb9c8c3bc482ecc10e0
Yoshi Rafael Gomez Gutierrez <32444740+bbfosho0@users.noreply.github.com>
2026-08-02 12:17:17 -0500

    test(frontend): provide shell HTTP dependencies

--- frontend/src/app/core/layout/app-shell.component.spec.ts
+++ frontend/src/app/core/layout/app-shell.component.spec.ts
@@ -1,3 +1,5 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { AppShellComponent } from './app-shell.component';
@@ -6,7 +8,7 @@ describe('AppShellComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppShellComponent],
-      providers: [provideRouter([])],
+      providers: [provideRouter([]), provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
   });
 
